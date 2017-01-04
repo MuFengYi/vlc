40a58985e06e8f2418ed2ba65adf2bd89577719c
CrazyYbf <18588264608@163.com>
2017-01-04 10:12:54 +0800

    Update libvlc-module.c

--- src/libvlc-module.c
+++ src/libvlc-module.c
@@ -1843,7 +1843,7 @@ vlc_module_begin ()
     add_obsolete_integer( "dvdnav-caching" ) /* 2.0.0 */
     add_obsolete_integer( "dvdread-caching" ) /* 2.0.0 */
     add_obsolete_integer( "vcd-caching" ) /* 2.0.0 */
-    add_integer( "network-caching", CLOCK_FREQ / 3000,
+    add_integer( "network-caching", CLOCK_FREQ / 5000,
                  NETWORK_CACHING_TEXT, NETWORK_CACHING_LONGTEXT, true )
         change_integer_range( 0, 60000 )
         change_safe()
